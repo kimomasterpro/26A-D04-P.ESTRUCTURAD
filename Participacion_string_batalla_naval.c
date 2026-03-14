832b01887e9ceeaf0aa323f521c75d654da198c8
kimomasterpro <aldo.rodriguez9761@alumnos.udg.mx>
2026-03-13 22:42:30 -0600

    Change ship name and update game messages

--- Participacion_string_batalla_naval.c
+++ Participacion_string_batalla_naval.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 
 int main() {
     int tablero[5][5];
     int fila, columna;
-    char jugador[20];
-    char barco[10] = "Destructor"; 
 
-    //Pedir nombre del jugador antes de iniciar
+    char jugador[20];
+
+    char barco[10] = "Fragata";
+
+    // pedir nombre del jugador
     printf("Nombre del jugador: ");
-    fgets(jugador, sizeof(jugador), stdin);  
-  
+    
+    fgets(jugador, sizeof(jugador), stdin);
+ 
     jugador[strcspn(jugador, "\n")] = 0;
 
     // Inicializar tablero
@@ -25,35 +28,11 @@ int main() {
     tablero[3][4] = 1;
     tablero[0][0] = 1;
 
-    // Mostrar tablero inicial
-    for(int i = 0; i < 5; i++) {
-        for(int j = 0; j < 5; j++) {
-            printf("~ ");
-        }
-        printf("\n");
-    }
-
     // Ciclo de turnos
     for(int turno = 0; turno < 5; turno++) {
-        //Mostrar nombre en cada turno
+        // Mostrar nombre en cada turno
         printf("\nTurno de %s\n", jugador);
 
-        printf("Fila: ");
-        scanf("%d", &fila);
-        printf("Columna: ");
-        scanf("%d", &columna);
-
-        // Evaluar si hay barco o agua
-        if (tablero[fila][columna] == 1) {
-            // mensaje personalizado cuando impacte 
-            printf("%s impactó un %s\n", jugador, barco);
-            tablero[fila][columna] = 3;
-        } else {
-            printf("agua\n");
-            tablero[fila][columna] = 2;
-        }
-
-        // actualización del tablero
         for(int i = 0; i < 5; i++) {
             for(int j = 0; j < 5; j++) {
                 if(tablero[i][j] == 0 || tablero[i][j] == 1) {
@@ -66,11 +45,25 @@ int main() {
             }
             printf("\n");
         }
+
+        printf("Fila: ");
+        scanf("%d", &fila);
+        printf("Columna: ");
+        scanf("%d", &columna);
+
+        // evaluar si hay barco o agua
+        if (tablero[fila][columna] == 1) {
+            //mostrar mensaje personalizado al impactar
+            printf("\n%s impacto un %s\n", jugador, barco);
+            tablero[fila][columna] = 3;
+        } else {
+            printf("\nAgua\n");
+            tablero[fila][columna] = 2;
+        }
     }
 
-    // mensaje final 
-    printf("\n%s termino la partida.\n", jugador);
+    // mostrar mensaje final
+    printf("\n%s gano la partida\n", jugador);
 
     return 0;
 }
-
